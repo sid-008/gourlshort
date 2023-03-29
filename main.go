8164220708080728c9532c33a5de52ea49db6ca6
Siddharth Tewari <siddharthtewari14@gmail.com>
2023-03-29 23:18:40 +0530

    Feat: Redirect from instore memory

--- main.go
+++ main.go
@@ -1,14 +1,33 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
+
+var (
+	store map[string]string
+)
 
 func main() {
-	//	store := make(map[string]string)
-
-	http.HandleFunc("/google", redirect)
+	store = map[string]string{
+		"twitter": "http://twitter.com",
+		"yahoo":   "http://yahoo.com",
+	}
+	http.HandleFunc("/lookup/", redirect)
+	log.Println("Server started on port 3000")
 	http.ListenAndServe(":3000", nil)
 }
 
 func redirect(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "http://google.com", 301)
+	req := r.URL.Query().Get("url")
+	//res := strings.Split(path, "/")
+	for key, value := range store {
+		if key == req {
+			http.Redirect(w, r, value, 307)
+		}
+	}
+	log.Println(req)
+
+	//http.Redirect(w, r, "http://google.com", 307)
 }
