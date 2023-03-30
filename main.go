24ba98ea74d236e4b694335eff0017a3395b3169
Siddharth Tewari <siddharthtewari14@gmail.com>
2023-03-30 22:00:08 +0530

    Feat: addlink endpoint create

--- main.go
+++ main.go
@@ -3,8 +3,25 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strings"
+	"time"
 )
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandString() string {
+	b := make([]rune, 8)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
 
 var (
 	store map[string]string
@@ -16,12 +33,33 @@ func main() {
 		"yahoo":   "http://yahoo.com",
 	}
 	http.HandleFunc("/lookup/", redirect)
+	http.HandleFunc("/add/", addlink)
 	log.Println("Server started on port 3000")
 	fmt.Println("POST /lookup/?url=YOUR_STRING_HERE")
-	http.ListenAndServe(":3000", nil)
+	fmt.Println("POST /add/?url=YOUR_STRING_HERE")
+	//str := RandString()
+	//fmt.Println(str)
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func addlink(w http.ResponseWriter, r *http.Request) {
+	req := r.URL.Query().Get("url")
+	short := RandString()
+	var req2 string
+	if !strings.HasPrefix(req, "http://") {
+		req2 = "http://" + req
+	}
+	store[short] = req2
+	log.Println(req)
+	log.Println(short)
+
 }
 
 func redirect(w http.ResponseWriter, r *http.Request) {
+	log.Println("store:", store)
 	req := r.URL.Query().Get("url")
 	//res := strings.Split(path, "/")
 	for key, value := range store {
